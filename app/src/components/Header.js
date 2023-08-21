ae568f59b390d9d963a0980b0d316827b0e75e67
armitage <scott@nihilanth.co.uk>
2023-08-21 20:14:26 +0100

    icon buttons

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,43 +1,70 @@
+import { FaUser, FaLanguage, FaShoppingCart } from 'react-icons/fa';
 import Container from 'react-bootstrap/Container';
-import '../styling/header.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, Outlet } from 'react-router-dom';
+import '../styling/header.css';
 
 const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">My App</Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link className="navbar-brand" to="/">
+                        My App
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <Link className="nav-link" to="/category">Category</Link>
+                                <Link className="nav-link" to="/category">
+                                    Category
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/category/men">Men</Link>
+                                <Link className="nav-link" to="/category/men">
+                                    Men
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/category/women">Women</Link>
+                                <Link className="nav-link" to="/category/women">
+                                    Women
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/category/jewellery">Jewellery</Link>
+                                <Link className="nav-link" to="/category/jewellery">
+                                    Jewellery
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/category/electronics">Electronics</Link>
+                                <Link className="nav-link" to="/category/electronics">
+                                    Electronics
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/signin">Sign In</Link>
+                                <Link className="nav-link" to="/signin">
+                                    Sign In
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/language">Language</Link>
+                                <a className="nav-link" href="/language">
+                                    <FaLanguage />
+                                </a>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/cart">Cart</Link>
+                                <a className="nav-link" href="/cart">
+                                    <FaShoppingCart />
+                                </a>
                             </li>
                         </ul>
                     </div>
